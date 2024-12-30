c0c69d328459c50d600e8ed9e25b9df559e4da57
RohithAchar <rohithachar2000@gmail.com>
2024-12-30 19:42:23 +0530

    working

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import WebSocket, { WebSocketServer } from "ws";
 import http from "http";
 
 const server = http.createServer();
+
 const wss = new WebSocketServer({ server: server });
 
 type DB = {
@@ -25,46 +26,8 @@ type Song = {
 
 let db: DB = {};
 
-// Cleanup function to remove unused rooms
-const cleanUpRooms = () => {
-  for (const roomId in db) {
-    const room = db[roomId];
-    room.users = room.users.filter(
-      (user) => user.readyState === WebSocket.OPEN
-    );
-
-    if (room.users.length === 0) {
-      console.log(`Deleting unused room: ${roomId}`);
-      delete db[roomId];
-    }
-  }
-};
-
-// Run cleanup every minute
-setInterval(cleanUpRooms, 60000);
-
 wss.on("connection", function connection(ws) {
   ws.on("error", console.error);
-
-  ws.on("close", () => {
-    for (const roomId in db) {
-      const room = db[roomId];
-      room.users = room.users.filter((user) => user !== ws);
-
-      if (room.admin === ws) {
-        // If the admin disconnects, transfer admin rights to another user or remove the room
-        if (room.users.length > 0) {
-          room.admin = room.users[0];
-          console.log(
-            `Admin disconnected. Transferring admin rights in room: ${roomId}`
-          );
-        } else {
-          console.log(`No users left in room: ${roomId}. Deleting room.`);
-          delete db[roomId];
-        }
-      }
-    }
-  });
 
   ws.on("message", function message(data) {
     const message = JSON.parse(data.toString());
@@ -72,7 +35,7 @@ wss.on("connection", function connection(ws) {
       console.log("create room");
       const { id, name } = message.data;
       if (db[id]) {
-        db[id].users.push(ws);
+        db[id].users[0] = ws;
         const activeSong = db[id].songs.find((song) => song.isActive);
         if (activeSong) {
           ws.send(JSON.stringify({ type: "active_song", data: activeSong }));
@@ -109,6 +72,7 @@ wss.on("connection", function connection(ws) {
         );
       }
 
+      // Check if the song already exists in the room
       const songExists = db[roomId].songs.some(
         (existingSong) => existingSong.id === song.id
       );
@@ -122,6 +86,7 @@ wss.on("connection", function connection(ws) {
         );
       }
 
+      // If the room has no songs, add the song and set it as active
       if (db[roomId].songs.length === 0) {
         db[roomId].songs.push({ ...song, votes: [], isActive: true });
         db[roomId].users.forEach((user) => {
@@ -166,9 +131,52 @@ wss.on("connection", function connection(ws) {
           JSON.stringify({ type: "song_updated", data: db[roomId].songs })
         );
       });
+    } else if (message.type === "down_vote") {
+      console.log("down vote");
+      const { roomId, songId, userId } = message.data;
+      if (!db[roomId]) {
+        return ws.send(
+          JSON.stringify({ type: "error", data: "room does not exist" })
+        );
+      }
+      const song = db[roomId].songs.find((song) => song.id === songId);
+      if (!song) {
+        return ws.send(
+          JSON.stringify({ type: "error", data: "song does not exist" })
+        );
+      }
+      if (!song.votes.includes(userId)) {
+        return ws.send(
+          JSON.stringify({ type: "error", data: "you are not voted this song" })
+        );
+      }
+      song.votes = song.votes.filter((vote) => vote !== userId);
+      db[roomId].songs.sort((a, b) => b.votes.length - a.votes.length);
+      db[roomId].users.forEach((user) => {
+        user.send(
+          JSON.stringify({ type: "song_updated", data: db[roomId].songs })
+        );
+      });
+    } else if (message.type === "on_end") {
+      console.log("on end");
+      const { roomId } = message.data;
+      db[roomId].songs = db[roomId].songs.filter((song) => !song.isActive);
+      if (db[roomId].songs.length > 0) {
+        const activeSong = db[roomId].songs.sort(
+          (a, b) => b.votes.length - a.votes.length
+        )[0];
+        activeSong.isActive = true;
+        db[roomId].users.forEach((user) => {
+          user.send(JSON.stringify({ type: "active_song", data: activeSong }));
+        });
+      }
+      db[roomId].users.forEach((user) => {
+        user.send(
+          JSON.stringify({ type: "song_updated", data: db[roomId].songs })
+        );
+      });
     }
   });
-
   console.log("connected");
   ws.send("Connected!");
 });
